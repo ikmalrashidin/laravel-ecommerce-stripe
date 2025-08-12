c454899668346e84449f610353181d731693db5f
ikmalrashidin <ikmalhishampro@gmail.com>
2025-08-12 18:06:28 +0800

    imported components and added a submit function

--- backend/src/views/Login.vue
+++ backend/src/views/Login.vue
@@ -1,22 +1,9 @@
 <template>
-  <!--
-    This example requires updating your template:
-
-    ```
-    <html class="h-full bg-white dark:bg-gray-900">
-    <body class="h-full">
-    ```
-  -->
-  <div class="flex min-h-full flex-1 flex-col justify-center px-6 py-12 lg:px-8">
-    <div class="sm:mx-auto sm:w-full sm:max-w-sm">
-      <!-- <img class="mx-auto h-10 w-auto dark:hidden" src="https://tailwindcss.com/plus-assets/img/logos/mark.svg?color=indigo&shade=600" alt="Your Company" /> -->
-      <h2 class="mt-10 text-center text-3xl font-bold tracking-tight text-gray-900 dark:text-white">Sign in to your account</h2>
-    </div>
-
-    <div class="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
-      <form class="space-y-6" action="#" method="POST">
+  <GuestLayout title="Sign in to your account" @submit="login">
+    <div class="mt-10 sm:mx-auto sm:w-full sm:max-w-sm space-y-6 ">
+      <!-- <form class="space-y-6" @submit.prevent="onSubmit"> -->
         <div>
-          <label for="email" class="block text-sm/6 font-medium text-gray-900 dark:text-gray-100 text-left">Email address</label>
+          <label for="email" class="block text-sm font-medium text-gray-900 dark:text-gray-100 text-left">Email address</label>
           <div class="mt-2">
             <input type="email" name="email" id="email" autocomplete="email" required class="block w-full rounded-md bg-white px-3 py-1.5 text-base text-gray-900  outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600 sm:text-sm/6 dark:bg-white/5 dark:text-white dark:outline-white/10 dark:placeholder:text-gray-500 dark:focus:outline-indigo-500" />
           </div>
@@ -24,26 +11,50 @@
 
         <div>
           <div class="flex items-center justify-between">
-            <label for="password" class="block text-sm/6 font-medium text-gray-900 dark:text-gray-100">Password</label>
+            <label for="password" class="block text-sm font-medium text-gray-900 dark:text-gray-100">Password</label>
             <div class="text-sm">
               <router-link :to="{name: 'requestPassword'}" class="font-semibold text-indigo-600 hover:text-indigo-500 dark:text-indigo-400 dark:hover:text-indigo-300">Forgot password?</router-link>
             </div>
           </div>
+
           <div class="mt-2">
             <input type="password" name="password" id="password" autocomplete="current-password" required class="block w-full rounded-md bg-white px-3 py-1.5 text-base text-gray-900  outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400  focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600 sm:text-sm/6 dark:bg-white/5 dark:text-white dark:outline-white/10 dark:placeholder:text-gray-500 dark:focus:outline-indigo-500" />
           </div>
         </div>
 
-        <div>
-          <button type="submit" class="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm/6 font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600 dark:bg-indigo-500 dark:shadow-none dark:hover:bg-indigo-400 dark:focus-visible:outline-indigo-500">Sign in</button>
+        <div class="flex items-center">
+          <input type="checkbox" id="remember-me" />
+          <label for="remember-me" class="ml-2 text-sm text-gray-900 dark:text-gray-100">Remember me</label>
         </div>
-      </form>
 
-      <p class="mt-10 text-center text-sm/6 text-gray-500 dark:text-gray-400">
-        Not a member?
-        {{ ' ' }}
-        <a href="#" class="font-semibold text-indigo-600 hover:text-indigo-500 dark:text-indigo-400 dark:hover:text-indigo-300">Start a 14 day free trial</a>
-      </p>
+        <div>
+          <button type="submit" class="flex w-full justify-center gap-2 rounded-md bg-indigo-600 px-3 py-1.5 text-sm/6 font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600 dark:bg-indigo-500 dark:shadow-none dark:hover:bg-indigo-400 dark:focus-visible:outline-indigo-500">
+            <LockClosedIcon class="h-5 w-5 text-indigo-500" aria-hidden="true" />
+            Sign in</button>
+        </div>
+      <!-- </form> -->
     </div>
-  </div>
+
+  </GuestLayout>
+
 </template>
+
+<script setup>
+
+import {ref} from 'vue'
+import GuestLayout from "../components/GuestLayout.vue";
+import { LockClosedIcon } from '@heroicons/vue/20/solid';
+
+const email = ref ('')
+const password = ref('')
+
+
+function login() {
+  console.log("Login")
+  //TODO: auth logic
+  // console.log({ email: email.value, password: password.value})
+}
+</script>
+
+
+<style scoped></style>
